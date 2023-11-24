271e71d3fdd1c324f3d04cd4f84910d3074b011d
Huỳnh Vỹ Hào <97943392+HuynhVyHao@users.noreply.github.com>
2023-11-24 18:03:23 +0700

    update trang TK va mathedata

--- screens/MaTheDATA.js
+++ screens/MaTheDATA.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,309 +7,291 @@ import {
   Image,
   TextInput,
 } from "react-native";
-import React, { useState } from "react";
 
 const MaTheDATA = ({ navigation }) => {
   const [selected, setSelected] = useState(1);
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          height: "12%",
-          backgroundColor: "#FF6600",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginLeft: 10,
-          }}
-        >
-          <Pressable
-            onPress={() => {
-              navigation.goBack();
-            }}
-          >
-            <Image
-              source={require("../assets/img/arrow-left.png")}
-              style={{ width: 40, height: 40 }}
-            />
-          </Pressable>
-          <Text
-            style={{
-              color: "white", // Màu chữ trắng
-              fontSize: 20,
-              marginLeft: "70px",
-            }}
-          >
-            Mã thẻ/DATA
-          </Text>
-        </View>
-        <View
-          style={{
-            width: "350px",
-            height: "39px",
-            borderRadius: "22px",
-            backgroundColor: "rgba(217, 217, 217, 0.34)",
-            alignItems: "center",
-            flexDirection: "row",
-            alignSelf: "center",
-          }}
-        >
-          <Pressable
-            onPress={() => {
-              setSelected(1);
-            }}
-            style={{
-              width: "165px",
-              height: "30px",
-              borderRadius: "22px",
-              backgroundColor:
-                selected == 1 ? "#FFF" : "rgba(217, 217, 217, 0.0)",
-              justifyContent: "center",
-              marginLeft: "5px",
-            }}
-          >
-            <Text
-              style={{
-                color: selected == 1 ? "#E77710" : "#D9D9D9",
-                fontSize: "13px",
-                fontWeight: 400,
-                textAlign: "center",
-              }}
-            >
-              Mua mã thẻ
-            </Text>
-          </Pressable>
-
-          <Pressable
-            onPress={() => {
-              setSelected(2);
-            }}
-            style={{
-              width: "165px",
-              height: "30px",
-              borderRadius: "22px",
-              backgroundColor:
-                selected == 2 ? "#FFF" : "rgba(217, 217, 217, 0.0)",
-              justifyContent: "center",
-              marginLeft: "10px",
-            }}
-          >
-            <Text
-              style={{
-                color: selected == 2 ? "#E77710" : "#D9D9D9",
-                fontSize: "13px",
-                fontWeight: 400,
-                textAlign: "center",
-              }}
-            >
-              Nạp DATA
-            </Text>
-          </Pressable>
-        </View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          width: 350,
-          height: 150,
-          borderRadius: 10,
-          alignSelf: "center",
-          marginTop: 20,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-            marginLeft: 10,
-            marginBottom: 5,
+      <View style={styles.header}>
+        <Pressable
+          onPress={() => {
+            navigation.goBack();
           }}
         >
           <Image
-            style={{ width: "24px", height: "24px" }}
+            source={require("../assets/img/arrow-left.png")}
+            style={styles.backButton}
+          />
+        </Pressable>
+        <Text style={styles.headerText}>Mã thẻ/DATA</Text>
+      </View>
+
+      <View style={styles.tabContainer}>
+        <Pressable
+          onPress={() => {
+            setSelected(1);
+          }}
+          style={[
+            styles.tabButton,
+            {
+              backgroundColor:
+                selected === 1 ? "#FFF" : "rgba(217, 217, 217, 0.0)",
+            },
+          ]}
+        >
+          <Text
+            style={[
+              styles.tabText,
+              { color: selected === 1 ? "#E77710" : "#D9D9D9" },
+            ]}
+          >
+            Mua mã thẻ
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => {
+            setSelected(2);
+          }}
+          style={[
+            styles.tabButton,
+            {
+              backgroundColor:
+                selected === 2 ? "#FFF" : "rgba(217, 217, 217, 0.0)",
+              marginLeft: 10,
+            },
+          ]}
+        >
+          <Text
+            style={[
+              styles.tabText,
+              { color: selected === 2 ? "#E77710" : "#D9D9D9" },
+            ]}
+          >
+            Nạp DATA
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.infoContainer}>
+        <View style={styles.infoHeader}>
+          <Image
+            style={styles.infoIcon}
             source={require("../assets/img/icon-thuhuong.png")}
           />
-          <Text style={{ fontWeight: 600, fontSize: "18px" }}>
-            Tài khoản nguồn
-          </Text>
+          <Text style={styles.infoHeaderText}>Tài khoản nguồn</Text>
         </View>
-        <Text
-          style={{
-            fontWeight: 600,
-            fontSize: "18px",
-            color: "#ff6600",
-            marginBottom: 20,
-            marginLeft: 10,
-          }}
-        >
-          55555555555
-        </Text>
-        <View
-          style={{
-            width: "95%",
-            height: 1,
-            backgroundColor: "#ccc",
-            alignSelf: "center",
-          }}
-        />
-        <View style={{ flexDirection: "row", marginTop: 20 }}>
-          <Text
-            style={{
-              fontWeight: 600,
-              fontSize: "18px",
-              marginLeft: 10,
-              color: "#91917e",
-            }}
-          >
-            Số dư
-          </Text>
-          <View style={{ flexDirection: "row", marginLeft: 190 }}>
-            <Text style={{ fontWeight: 600, fontSize: "18px" }}>0 VND</Text>
-
+        <Text style={styles.infoText}>55555555555</Text>
+        <View style={styles.infoDivider} />
+        <View style={styles.infoRow}>
+          <Text style={styles.infoLabel}>Số dư</Text>
+          <View style={styles.infoBalanceContainer}>
+            <Text style={styles.infoBalance}>0 VND</Text>
             <Image
-              style={{
-                tintColor: "#ff6600",
-                width: "24px",
-                height: "24px",
-                marginLeft: 10,
-              }}
+              style={styles.eyeIcon}
               source={require("../assets/img/eye-crossed.png")}
             />
           </View>
         </View>
       </View>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          width: 350,
-          height: 250,
-          borderRadius: 10,
-          alignSelf: "center",
-          marginTop: 20,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-            marginLeft: 10,
-            marginBottom: 5,
-          }}
-        >
+
+      <View style={styles.cardInfoContainer}>
+        <View style={styles.cardInfoHeader}>
           <Image
-            style={{ tintColor: "#ff6600", width: "24px", height: "24px" }}
+            style={styles.cardInfoIcon}
             source={require("../assets/img/usd.png")}
           />
-          <Text style={{ fontWeight: 600, fontSize: "18px" }}>
-            Thông tin mã thẻ
-          </Text>
+          <Text style={styles.cardInfoHeaderText}>Thông tin mã thẻ</Text>
         </View>
-        <TextInput
-          style={{
-            marginTop: 20,
-            color: "#91917e",
-            fontSize: "18px",
-            fontWeight: 600,
-            marginLeft: 10,
-          }}
-          placeholder="Thẻ điện thoại"
-        />
-        <View
-          style={{
-            width: "95%",
-            height: 1,
-            backgroundColor: "#ccc",
-            alignSelf: "center",
-          }}
-        />
-        <TextInput
-          style={{
-            marginTop: 30,
-            color: "#91917e",
-            fontSize: "18px",
-            fontWeight: 600,
-            marginLeft: 10,
-          }}
-          placeholder="Nhà cung cấp"
-        />
-        <View
-          style={{
-            width: "95%",
-            height: 1,
-            backgroundColor: "#ccc",
-            alignSelf: "center",
-          }}
-        />
-        <TextInput
-          style={{
-            marginTop: 30,
-            color: "#91917e",
-            fontSize: "18px",
-            fontWeight: 600,
-            marginLeft: 10,
-          }}
-          placeholder="Mệnh giá"
-        />
-        <View
-          style={{
-            width: "95%",
-            height: 1,
-            backgroundColor: "#ccc",
-            alignSelf: "center",
-          }}
-        />
+        <TextInput style={styles.cardInfoInput} placeholder="Thẻ điện thoại" />
+        <View style={styles.infoDivider} />
+        <TextInput style={styles.cardInfoInput} placeholder="Nhà cung cấp" />
+        <View style={styles.infoDivider} />
+        <TextInput style={styles.cardInfoInput} placeholder="Mệnh giá" />
       </View>
-      <View style={{ flexDirection: "row", marginLeft: 10, marginTop: 20 }}>
+
+      <View style={styles.buttonContainer}>
         <Pressable
           onPress={() => {
             navigation.goBack();
           }}
-          style={{
-            borderColor: "#ff6600",
-            borderWidth: 3,
-            borderRadius: 30,
-            width: 170,
-            height: 50,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+          style={styles.cancelButton}
         >
-          <Text style={{ color: "#ff6600", fontWeight: 700, fontSize: "18px" }}>
-            Hủy
-          </Text>
+          <Text style={styles.buttonText}>Hủy</Text>
         </Pressable>
-        <Pressable
-          style={{
-            borderColor: "#ff6600",
-            backgroundColor: "#ff6600",
-            borderWidth: 3,
-            borderRadius: 30,
-            width: 170,
-            height: 50,
-            alignItems: "center",
-            justifyContent: "center",
-            marginLeft: 20,
-          }}
-        >
-          <Text style={{ color: "#fff", fontWeight: 700, fontSize: "18px" }}>
-            Tiếp tục
-          </Text>
+        <Pressable style={styles.continueButton}>
+          <Text style={styles.buttonText}>Tiếp tục</Text>
         </Pressable>
       </View>
     </View>
   );
 };
-
 export default MaTheDATA;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ccc",
   },
+  header: {
+    height: "12%",
+    backgroundColor: "#FF6600",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+  },
+  backButton: {
+    width: 40,
+    height: 40,
+  },
+  headerText: {
+    color: "white",
+    fontSize: 20,
+    marginLeft: "70px",
+  },
+  tabContainer: {
+    width: "350px",
+    height: "39px",
+    borderRadius: "22px",
+    backgroundColor: "rgba(217, 217, 217, 0.34)",
+    alignItems: "center",
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  tabButton: {
+    width: "165px",
+    height: "30px",
+    borderRadius: "22px",
+    justifyContent: "center",
+    marginLeft: "5px",
+  },
+  tabText: {
+    fontSize: "13px",
+    fontWeight: 400,
+    textAlign: "center",
+  },
+  infoContainer: {
+    backgroundColor: "#fff",
+    width: 350,
+    height: 150,
+    borderRadius: 10,
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  infoHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  infoIcon: {
+    width: "24px",
+    height: "24px",
+  },
+  infoHeaderText: {
+    fontWeight: 600,
+    fontSize: "18px",
+  },
+  infoText: {
+    fontWeight: 600,
+    fontSize: "18px",
+    color: "#ff6600",
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  infoDivider: {
+    width: "95%",
+    height: 1,
+    backgroundColor: "#ccc",
+    alignSelf: "center",
+  },
+  infoRow: {
+    flexDirection: "row",
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  infoLabel: {
+    fontWeight: 600,
+    fontSize: "18px",
+    color: "#91917e",
+  },
+  infoBalanceContainer: {
+    flexDirection: "row",
+    marginLeft: 190,
+  },
+  infoBalance: {
+    fontWeight: 600,
+    fontSize: "18px",
+  },
+  eyeIcon: {
+    tintColor: "#ff6600",
+    width: "24px",
+    height: "24px",
+    marginLeft: 10,
+  },
+  cardInfoContainer: {
+    backgroundColor: "#fff",
+    width: 350,
+    height: 250,
+    borderRadius: 10,
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  cardInfoHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  cardInfoIcon: {
+    tintColor: "#ff6600",
+    width: "24px",
+    height: "24px",
+  },
+  cardInfoHeaderText: {
+    fontWeight: 600,
+    fontSize: "18px",
+  },
+  cardInfoInput: {
+    marginTop: 20,
+    color: "#91917e",
+    fontSize: "18px",
+    fontWeight: 600,
+    marginLeft: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginLeft: 10,
+    marginTop: 20,
+  },
+  cancelButton: {
+    borderColor: "#ff6600",
+    borderWidth: 3,
+    borderRadius: 30,
+    width: 170,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  continueButton: {
+    borderColor: "#ff6600",
+    backgroundColor: "#ff6600",
+    borderWidth: 3,
+    borderRadius: 30,
+    width: 170,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: 700,
+    fontSize: "18px",
+  },
 });
+
+
--- screens/TaiKhoan.js
+++ screens/TaiKhoan.js
@@ -1,3 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Pressable,
+  Modal,
+} from "react-native";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+LocaleConfig.locales["vi"] = {
+  monthNames: [
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
+    "Tháng 7",
+    "Tháng 8",
+    "Tháng 9",
+    "Tháng 10",
+    "Tháng 11",
+    "Tháng 12",
+  ],
+  monthNamesShort: [
+    "Th.1",
+    "Th.2",
+    "Th.3",
+    "Th.4",
+    "Th.5",
+    "Th.6",
+    "Th.7",
+    "Th.8",
+    "Th.9",
+    "Th.10",
+    "Th.11",
+    "Th.12",
+  ],
+  dayNames: [
+    "Chủ Nhật",
+    "Thứ Hai",
+    "Thứ Ba",
+    "Thứ Tư",
+    "Thứ Năm",
+    "Thứ Sáu",
+    "Thứ Bảy",
+  ],
+  dayNamesShort: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+};
 LocaleConfig.defaultLocale = "vi";
 
 const TaiKhoan = ({ navigation }) => {
