ed63b20d64bf6708530262b2431111d674d8d9cc
Huỳnh Vỹ Hào <97943392+HuynhVyHao@users.noreply.github.com>
2023-11-24 17:42:15 +0700

    hoan chinh trang TK

--- screens/TrangTK.js
+++ screens/TrangTK.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -6,16 +7,17 @@ import {
   TouchableOpacity,
   Pressable,
 } from "react-native";
-import React from "react";
 
 const TrangTK = ({ navigation, route }) => {
-  const { id, item } = route.params;
+  const { item } = route.params;
+
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
-      currency: "USD", // You can change the currency code as needed
+      currency: "USD",
     }).format(amount);
   };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -26,7 +28,7 @@ const TrangTK = ({ navigation, route }) => {
         >
           <Image
             source={require("../assets/img/arrow-left.png")}
-            style={{ width: 40, height: 40, right: 45 }}
+            style={styles.backIcon}
           />
         </Pressable>
         <Text style={styles.headerText}>THÔNG TIN KHÁCH HÀNG</Text>
@@ -36,17 +38,15 @@ const TrangTK = ({ navigation, route }) => {
         <View style={styles.paymentInfoContainer}>
           <Image
             source={require("../assets/img/itemcard.jpg")}
-            style={{ width: 30, height: 30 }}
+            style={styles.paymentIcon}
           />
           <Text style={styles.paymentInfoText}> Tài khoản thanh toán</Text>
         </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={{ left: 15, color: "#FF6600", fontWeight: 700 }}>
-            {item.SoTK}
-          </Text>
+        <View style={styles.accountInfo}>
+          <Text style={styles.accountNumber}>{item.SoTK}</Text>
           <Image
             source={require("../assets/img/copy.jpg")}
-            style={{ top: 2.5, left: 20, width: 20, height: 20 }}
+            style={styles.copyIcon}
           />
           <Pressable
             onPress={() => {
@@ -55,57 +55,33 @@ const TrangTK = ({ navigation, route }) => {
           >
             <Image
               source={require("../assets/img/qr.png")}
-              style={{
-                width: 24,
-                height: 24,
-                tintColor: "#ff6600",
-                marginLeft: 200,
-              }}
+              style={styles.qrIcon}
             />
           </Pressable>
         </View>
-        <Text style={{ left: 15, fontWeight: 700 }}>
-          Số dư: {formatCurrency(item.SoDu)}
-        </Text>
+        <Text style={styles.balance}>Số dư: {formatCurrency(item.SoDu)}</Text>
       </View>
-      <View
-        style={{
-          flexDirection: "row",
 
-          borderRadius: 20,
-          backgroundColor: "#fff",
-          width: 370,
-          height: 60,
-          marginLeft: 10,
-        }}
-      >
+      <View style={styles.savingsAccount}>
         <Image
           source={require("../assets/img/pig.jpg")}
-          style={{ width: 30, height: 30, top: 10, left: 5 }}
+          style={styles.savingsIcon}
         />
-        <Text style={{ left: 10, top: 15 }}> Tài khoản tiết kiệm</Text>
-        <Text
-          style={{
-            left: 100,
-            top: 15,
-            color: "#FF6600",
-            textDecorationLine: "underline",
-            fontStyle: "italic",
-          }}
-        >
-          {" "}
-          Xem chi tiết{" "}
-        </Text>
+        <Text style={styles.savingsText}> Tài khoản tiết kiệm</Text>
+        <Text style={styles.detailsLink}>Xem chi tiết</Text>
       </View>
 
-      <TouchableOpacity style={styles.bottomButton} onPress={() => {}}>
+      <TouchableOpacity
+        style={styles.bottomButton}
+        onPress={() => {
+          // Thêm xử lý khi nhấn vào nút
+        }}
+      >
         <Text style={styles.buttonText}>Quản lý nickname tài khoản</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-export default TrangTK;
 
 const styles = StyleSheet.create({
   container: {
@@ -113,15 +89,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
   },
   header: {
-    backgroundColor: "#FF6600", // Màu cam
+    backgroundColor: "#FF6600",
     padding: 15,
     width: "100%",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
   },
+  backIcon: {
+    width: 40,
+    height: 40,
+    right: 45,
+  },
   headerText: {
-    color: "white", // Màu chữ trắng
+    color: "white",
     fontSize: 20,
   },
   mainContent: {
@@ -131,23 +112,80 @@ const styles = StyleSheet.create({
     width: 370,
     height: 100,
     marginVertical: 10,
-
     left: 10,
+    padding: 10,
   },
   paymentInfoContainer: {
     flexDirection: "row",
     borderBottomWidth: 1,
     borderStyle: "dashed",
-    width: "100%",
     borderColor: "gray",
     alignItems: "center",
     marginBottom: 10,
   },
-
+  paymentIcon: {
+    width: 30,
+    height: 30,
+  },
+  paymentInfoText: {
+    marginLeft: 10,
+  },
+  accountInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  accountNumber: {
+    left: 15,
+    color: "#ff6600",
+    fontWeight: "bold",
+  },
+  copyIcon: {
+    top: 2.5,
+    left: 20,
+    width: 20,
+    height: 20,
+  },
+  qrIcon: {
+    width: 24,
+    height: 24,
+    tintColor: "#ff6600",
+    marginLeft: 200,
+  },
+  balance: {
+    left: 15,
+    fontWeight: "bold",
+  },
+  savingsAccount: {
+    flexDirection: "row",
+    borderRadius: 20,
+    backgroundColor: "#fff",
+    width: 370,
+    height: 60,
+    marginLeft: 10,
+    alignItems: "center",
+    marginTop: 10,
+    padding: 10,
+  },
+  savingsIcon: {
+    width: 30,
+    height: 30,
+    top: 10,
+    left: 5,
+  },
+  savingsText: {
+    left: 10,
+    top: 15,
+  },
+  detailsLink: {
+    left: 100,
+    top: 15,
+    color: "#FF6600",
+    textDecorationLine: "underline",
+    fontStyle: "italic",
+  },
   bottomButton: {
     position: "absolute",
     bottom: 20,
-
     backgroundColor: "#FF6600",
     paddingVertical: 10,
     paddingHorizontal: 40,
@@ -159,3 +197,5 @@ const styles = StyleSheet.create({
     color: "white",
   },
 });
+
+export default TrangTK;
